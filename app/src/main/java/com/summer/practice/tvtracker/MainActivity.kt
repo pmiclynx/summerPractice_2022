e6a0dbe02545e4a7b736390e57040557565113ab
Patricia Mic <patricia.mic@lynxsolutions.eu>
2022-07-07 10:18:34 +0300

    #14 Demo Pull Request: Remove log.

--- app/src/main/java/com/summer/practice/tvtracker/MainActivity.kt
+++ app/src/main/java/com/summer/practice/tvtracker/MainActivity.kt
@@ -1,13 +1,11 @@
 package com.summer.practice.tvtracker
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Log.i("AAAAAAA", "Hello World!")
     }
 }
