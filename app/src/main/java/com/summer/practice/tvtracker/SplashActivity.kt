afff17b7b50c2faf402b5c6e0f77a78dcf390b94
denisabodnar <denisabodnar24@yahoo.com>
2022-07-12 11:06:17 +0300

    #1 Create Splash Screen: Add ability to display the splash screen for 2 seconds.

--- app/src/main/java/com/summer/practice/tvtracker/SplashActivity.kt
+++ app/src/main/java/com/summer/practice/tvtracker/SplashActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import androidx.appcompat.app.AppCompatActivity
+import com.summer.practice.tvtracker.login.LoginActivity
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?){
@@ -12,7 +13,7 @@ class SplashActivity : AppCompatActivity() {
             supportActionBar?.hide();
 
             Handler().postDelayed({
-                //val intent= Intent(this@SplashActivity, Register)
+                val intent= Intent(this@SplashActivity, LoginActivity::class.java)
                 startActivity(intent)
                 finish()
             },2000)
