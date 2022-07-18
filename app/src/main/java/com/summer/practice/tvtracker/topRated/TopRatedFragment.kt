8530dd6d5322e0bcad112fbd2d11525688f6af54
denesdavid1 <99787234+denesdavid1@users.noreply.github.com>
2022-07-18 12:55:58 +0300

    #5 Create Popular & Top Rated RecyclerView Adapter: Added Adapter 📈🤑👀

--- app/src/main/java/com/summer/practice/tvtracker/topRated/TopRatedFragment.kt
+++ app/src/main/java/com/summer/practice/tvtracker/topRated/TopRatedFragment.kt
@@ -1,13 +1,19 @@
-package com.summer.practice.tvtracker
+package com.summer.practice.tvtracker.topRated
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.summer.practice.tvtracker.databinding.FragmentPopularBinding
+import com.summer.practice.tvtracker.ExampleItem
+import com.summer.practice.tvtracker.R
 import com.summer.practice.tvtracker.databinding.FragmentTopRatedBinding
+import com.summer.practice.tvtracker.details.DetailsActivity
+import com.summer.practice.tvtracker.popular.Adapter
+
+
 
 class TopRatedFragment : Fragment() {
 
@@ -19,21 +25,32 @@ class TopRatedFragment : Fragment() {
 
         val exampleList = generateDummyList(50)
 
-        binding.recyclerViewTopRated.adapter = Adaptor(exampleList, requireContext())
+        binding.recyclerViewTopRated.adapter = Adapter(
+            exampleList,
+            object: Adapter.ItemClickListener {
+            override fun onItemClicked(id: Int) {
+                startActivity(Intent(context, DetailsActivity::class.java))
+            }
+
+        })
         binding.recyclerViewTopRated.layoutManager = LinearLayoutManager(context)
         binding.recyclerViewTopRated.setHasFixedSize(true)
 
         return binding.root
     }
 
-    private fun generateDummyList(size: Int): List<ExampleItem> {
+    private fun generateDummyList(size: Int): List<Movie> {
 
-        val list = ArrayList<ExampleItem>()
+        val list = ArrayList<Movie>()
 
         for (i in 0 until size) {
             val drawable = R.drawable.ic_baseline_local_movies_24
 
-            val item = ExampleItem(drawable, "#$i Top Rated Movie")
+            val item = Movie(
+                id = i,
+                title = "Top Rated $i",
+                imageUrl = "https://image.tmdb.org/t/p/w500/bQLrHIRNEkE3PdIWQrZHynQZazu.jpg?api_key=563e3b53b3c3a7da6ceae87959d74162"
+            )
             list += item
         }
 
