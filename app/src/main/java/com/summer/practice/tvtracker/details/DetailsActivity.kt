62271b2cbf6f881bf50d2434f6ee52a382ca3e02
DomiSzocs <domiszocs@gmail.com>
2022-07-20 11:05:31 +0300

    #13 Get data for Details Screen: Make work for a fixed id

--- app/src/main/java/com/summer/practice/tvtracker/details/DetailsActivity.kt
+++ app/src/main/java/com/summer/practice/tvtracker/details/DetailsActivity.kt
@@ -2,8 +2,14 @@ package com.summer.practice.tvtracker.details
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
 import android.widget.Toast
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.CenterCrop
+import com.summer.practice.tvtracker.R
 import com.summer.practice.tvtracker.databinding.ActivityDetailsBinding
+import com.summer.practice.tvtracker.networking.*
 
 class DetailsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -26,5 +32,34 @@ class DetailsActivity : AppCompatActivity() {
         binding.backArrow.setOnClickListener {
             super.onBackPressed()
         }
+
+        MoviesDetailRepository(66732).getDetail(
+            onSuccess = ::onPopularMoviesFetched,
+            onError = ::onError,
+        )
+    }
+
+    private fun onError(error: String) {
+        makeToast(this, error)
+    }
+
+    private fun onPopularMoviesFetched(detail: Detail) {
+        val pathList = createPathList(detail)
+        val binding = ActivityDetailsBinding.inflate(layoutInflater)
+        binding.title.text = pathList.name
+        binding.rating.text = pathList.voteAverage.toString()
+        binding.innerTextView.text = pathList.overview
+
+        Glide.with(binding.root)
+            .load(pathList.backdropPath)
+            .transform(CenterCrop())
+            .into(binding.backdropImage)
+
+        Glide.with(binding.root)
+            .load(pathList.posterPath)
+            .transform(CenterCrop())
+            .into(binding.coverImage)
+
+        setContentView(binding.root)
     }
 }
--- app/src/main/java/com/summer/practice/tvtracker/networking/TheMovieDbApi.kt
+++ app/src/main/java/com/summer/practice/tvtracker/networking/TheMovieDbApi.kt
@@ -1,5 +1,6 @@
 package com.summer.practice.tvtracker.networking
 
+import com.summer.practice.tvtracker.details.Detail
 import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Query
@@ -7,4 +8,7 @@ import retrofit2.http.Query
 interface TheMovieDbApi {
     @GET("tv")
     fun getList(@Query("page") page: Int = 1) : Call<Results>
+
+    @GET("tv")
+    fun getDetail() : Call<Detail>
 }
