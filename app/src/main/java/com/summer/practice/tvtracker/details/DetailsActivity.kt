04be27dc2964dda5f65fdbfebec4289a2705eee8
DomiSzocs <domiszocs@gmail.com>
2022-07-20 12:34:55 +0300

    #9 Get Data for Popular Screen: Populate both views

--- app/src/main/java/com/summer/practice/tvtracker/details/DetailsActivity.kt
+++ app/src/main/java/com/summer/practice/tvtracker/details/DetailsActivity.kt
@@ -1,13 +1,20 @@
 package com.summer.practice.tvtracker.details
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.summer.practice.tvtracker.databinding.ActivityDetailsBinding
+
 
 class DetailsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        val intent = intent
+        val id = intent.getIntExtra("id", 0)
+
+        Log.d("id:::::::::", id.toString())
 
         val binding = ActivityDetailsBinding.inflate(layoutInflater)
         val view = binding.root
