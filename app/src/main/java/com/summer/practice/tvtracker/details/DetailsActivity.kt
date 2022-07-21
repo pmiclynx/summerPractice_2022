e56f89318c8bac988cbde1bbfccecc28e5552d6d
DomiSzocs <domiszocs@gmail.com>
2022-07-21 16:45:04 +0300

    #12 Manage Favorites with Firebase Firestore: Ability to add movies to favorites

--- app/src/main/java/com/summer/practice/tvtracker/details/DetailsActivity.kt
+++ app/src/main/java/com/summer/practice/tvtracker/details/DetailsActivity.kt
@@ -1,17 +1,31 @@
 package com.summer.practice.tvtracker.details
 
+import android.os.Build
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.bitmap.CenterCrop
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 import com.summer.practice.tvtracker.databinding.ActivityDetailsBinding
 import com.summer.practice.tvtracker.networking.*
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
 
 
 class DetailsActivity : AppCompatActivity() {
-    lateinit var binding: ActivityDetailsBinding
+    private lateinit var binding: ActivityDetailsBinding
+    private val auth = FirebaseAuth.getInstance()
+    private val db = Firebase.firestore
 
+    private lateinit var backDropUrl: String
+    private lateinit var posterUrl: String
+
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -24,7 +38,26 @@ class DetailsActivity : AppCompatActivity() {
 
         binding.addToFavoritesButton.setOnClickListener {
 
-            //TODO add 'save as favorite' logic
+            val favorite = hashMapOf(
+                "id" to id.toString(),
+                "title" to binding.title.text,
+                "backdropUrl" to backDropUrl,
+                "posterUrl" to posterUrl,
+                "dateTimeStamp" to LocalDate.now().format(DateTimeFormatter.ofPattern("yyyy.MM.dd")).toString()
+            )
+
+            auth.uid?.let { it1 ->
+                db.collection("users")
+                    .document(it1)
+                    .collection("favorites")
+                    .add(favorite)
+                    .addOnSuccessListener {
+                        Log.d("TAG", "DocumentSnapshot added with ID:")
+                    }
+                    .addOnFailureListener { e ->
+                        Log.w("TAG", "Error adding document", e)
+                    }
+            }
 
             val text = "Saved to favorites"
             val duration = Toast.LENGTH_SHORT
@@ -51,6 +84,9 @@ class DetailsActivity : AppCompatActivity() {
         binding.title.text = pathList.name
         binding.rating.text = pathList.voteAverage.toString()
         binding.innerTextView.text = pathList.overview
+
+        backDropUrl = pathList.backdropPath.toString()
+        posterUrl = pathList.posterPath.toString()
 
         Glide.with(binding.root)
             .load(pathList.backdropPath)
