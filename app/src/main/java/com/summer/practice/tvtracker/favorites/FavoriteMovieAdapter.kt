a9dd6649082c5b4e00089e0b5e1404589651d675
denisabodnar <denisabodnar24@yahoo.com>
2022-07-26 09:48:00 +0300

    #28 Make all items visible in RecyclerView

--- app/src/main/java/com/summer/practice/tvtracker/favorites/FavoriteMovieAdapter.kt
+++ app/src/main/java/com/summer/practice/tvtracker/favorites/FavoriteMovieAdapter.kt
@@ -5,12 +5,14 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.summer.practice.tvtracker.R
 import com.summer.practice.tvtracker.databinding.ItemFavoriteMovieBinding
+import com.summer.practice.tvtracker.databinding.ItemLastFavoriteMovieBinding
 
 class FavoriteMovieAdapter(
     private val list: List<FavoriteMovie>,
     private val itemClickListener: ItemClickListener
-) : RecyclerView.Adapter<FavoriteMovieAdapter.MovieViewHolder>() {
+) : RecyclerView.Adapter<FavoriteMovieAdapter.GenericViewHolder>() {
 
     private val items: MutableList<FavoriteMovie> = list.toMutableList()
 
@@ -22,9 +24,16 @@ class FavoriteMovieAdapter(
         fun onItemDeleted(movie: FavoriteMovie)
     }
 
-    class MovieViewHolder(private val binding: ItemFavoriteMovieBinding) :
-        RecyclerView.ViewHolder(binding.root) {
-        fun bind(
+    abstract class GenericViewHolder(itemView: View?) :
+        RecyclerView.ViewHolder(itemView!!) {
+        abstract fun bind(movie: FavoriteMovie, itemClickListener: ItemClickListener,itemDeleteListener: ItemDeleteListener)
+
+    }
+
+    class MovieViewHolder(view: View, private val binding: ItemFavoriteMovieBinding) :
+        GenericViewHolder(binding.root) {
+
+        override fun bind(
             movie: FavoriteMovie,
             itemClickListener: ItemClickListener,
             itemDeleteListener: ItemDeleteListener
@@ -47,14 +56,50 @@ class FavoriteMovieAdapter(
         }
 
     }
+    class LastMovieViewHolder(view: View, private val binding: ItemLastFavoriteMovieBinding) :
+        GenericViewHolder(binding.root) {
+        override fun bind(
+            movie: FavoriteMovie,
+            itemClickListener: ItemClickListener,
+            itemDeleteListener: ItemDeleteListener
+        ) {
+            binding.textViewMovieTitle.text = movie.title
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MovieViewHolder {
-        return MovieViewHolder(
-            ItemFavoriteMovieBinding.inflate(LayoutInflater.from(parent.context), parent, false)
-        )
+            Glide.with(binding.root).load(movie.imageUrl).into(binding.imageViewMovie)
+
+            binding.root.setOnClickListener(object : View.OnClickListener {
+                override fun onClick(v: View?) {
+                    itemClickListener.onItemClicked(movie.id)
+                }
+            })
+
+            binding.textViewDelete.setOnClickListener {
+                itemDeleteListener.onItemDeleted(movie)
+            }
+
+            binding.textViewDate.text = movie.dateAdded.toString()
+        }
     }
 
-    override fun onBindViewHolder(holder: MovieViewHolder, position: Int) {
+    override fun getItemViewType(position: Int): Int {
+        return position
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): GenericViewHolder {
+        if(viewType == items.size-1){
+            val view: View = LayoutInflater.from(parent.context)
+                .inflate(R.layout.item_last_favorite_movie, parent, false)
+            return LastMovieViewHolder(view, ItemLastFavoriteMovieBinding.bind(view))
+        }
+        else {
+            val view: View = LayoutInflater.from(parent.context)
+                .inflate(R.layout.item_favorite_movie, parent, false)
+            return MovieViewHolder(view, ItemFavoriteMovieBinding.bind(view))
+        }
+
+    }
+
+    override fun onBindViewHolder(holder: GenericViewHolder, position: Int) {
         val pos = position
 
         holder.bind(items[position], itemClickListener, object : ItemDeleteListener {
